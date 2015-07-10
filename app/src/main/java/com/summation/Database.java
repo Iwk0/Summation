23ab8e362fa14e9d9095b9a1092fc7254b8d45fc
Iwk0 <ivomishev@gmail.com>
2015-07-10 14:06:03 +0300

    Update

--- app/src/main/java/com/summation/Database.java
+++ app/src/main/java/com/summation/Database.java
@@ -12,7 +12,14 @@ import java.util.List;
 public class Database extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "Summation";
-    private static final int DATABASE_VERSION = 8;
+    private static final int DATABASE_VERSION = 1;
+
+    /*Column names*/
+    private static final String TABLE_NAME = "Score";
+    private static final String ID = "id";
+    private static final String TIME = "time";
+    private static final String NAME = "name";
+    private static final String SUCCESSFUL_SUMMATION = "successful_summation";
 
     public Database(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -21,17 +28,17 @@ public class Database extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         String createTableLoans = String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, %s TEXT, %s TEXT, %s INTEGER);",
-                "Score",
-                "id",
-                "time",
-                "name",
-                "successful_summation");
+                TABLE_NAME,
+                ID,
+                TIME,
+                NAME,
+                SUCCESSFUL_SUMMATION);
         sqLiteDatabase.execSQL(createTableLoans);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS Score");
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + DATABASE_NAME);
         onCreate(sqLiteDatabase);
     }
 
@@ -39,17 +46,17 @@ public class Database extends SQLiteOpenHelper {
         SQLiteDatabase db = getWritableDatabase();
         ContentValues values = new ContentValues();
 
-        values.put("time", score.time);
-        values.put("name", score.name);
-        values.put("successful_summation", score.countSuccessfulSummation);
+        values.put(TIME, score.time);
+        values.put(NAME, score.name);
+        values.put(SUCCESSFUL_SUMMATION, score.countSuccessfulSummation);
 
-        db.insert("Score", null, values);
+        db.insert(TABLE_NAME, null, values);
         db.close();
     }
 
     public List<Score> getTopTwenty() {
         Cursor cursor = getWritableDatabase().rawQuery(String.format("SELECT * FROM %s ORDER BY %s DESC, %s ASC LIMIT 20;",
-                "Score", "successful_summation", "time"), new String[]{});
+                TABLE_NAME, SUCCESSFUL_SUMMATION, TIME), new String[]{});
 
         List<Score> scores = new ArrayList<>();
         if (cursor.moveToFirst()) {
